d63e010cafa24f66e9028140f779eaecd990d141
marcelodesouzacorrea <marcelosouzacorrea@hotmail.com>
2021-04-18 16:14:22 -0300

    Vincular variáveis ​​à instrução

--- read.php
+++ read.php
@@ -3,4 +3,11 @@
     if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
     // inclui o arquivo config
     require_once "config.php";
+
+     // Prepare uma declaração selecionada
+     $sql = "SELECT * FROM employees WHERE id = ?";
+
+     if($stmt = mysqli_prepare($link, $sql)){
+        // Vincular variáveis ​​à instrução preparada como parâmetros
+        mysqli_stmt_bind_param($stmt, "i", $param_id);
 ?>
