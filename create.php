356a83d477c574335960cc17a13a5fa9209a2119
marcelodesouzacorrea <marcelosouzacorrea@hotmail.com>
2021-04-18 15:18:33 -0300

    valida os inputs

--- create.php
+++ create.php
@@ -17,4 +17,49 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     } else{
         $nome = $input_nome;
     }
+    // validando o inicio
+    $input_inicio = trim($_POST["inicio"]);
+    if(empty($input_inicio)){
+        $inicio_err = "Digite o inicio.";
+    } elseif(!filter_var($input_inicio, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+        $inicio_err = "Digite um inicio valido.";
+    } else{
+        $inicio = $input_inicio;
+    }
+    // validando o fim
+    $input_fim = trim($_POST["fim"]);
+    if(empty($input_fim)){
+        $fim_err = "Digite o fim.";
+    } elseif(!filter_var($input_fim, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+        $fim_err = "Digite um fim valido.";
+    } else{
+        $fim = $input_fim;
+    }
+    // validando o valor
+    $input_valor = trim($_POST["valor"]);
+    if(empty($input_valor)){
+        $name_err = "Digite o valor.";
+    } elseif(!filter_var($input_valor, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+        $valor_err = "Digite um valor valido.";
+    } else{
+        $valor = $input_valor;
+    }
+    // validando o riscos
+    $input_riscos = trim($_POST["riscos"]);
+    if(empty($input_riscos)){
+        $name_err = "Digite o riscos.";
+    } elseif(!filter_var($input_riscos, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+        $riscos_err = "Digite um riscos valido.";
+    } else{
+        $riscos = $input_riscos;
+    }
+    // validando o participantes
+    $input_participantes = trim($_POST["participantes"]);
+    if(empty($input_participantes)){
+        $participantes_err = "Digite o participantes.";
+    } elseif(!filter_var($input_participantes, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+        $participantes_err = "Digite um participantes valido.";
+    } else{
+        $participantes = $input_participantes;
+    }
 ?>
