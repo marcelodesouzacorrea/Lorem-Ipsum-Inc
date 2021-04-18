0327b2a72a7d796393de9013d18ae2be45101c73
marcelodesouzacorrea <marcelosouzacorrea@hotmail.com>
2021-04-18 14:34:18 -0300

    /checar conexão

--- config.php
+++ config.php
@@ -7,5 +7,8 @@
 
     // Tentativa de conexão ao banco de dados MySQL
     $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-
+    //checar conexão
+    if($link === false){
+        die("ERROR: Could not connect. " . mysqli_connect_error());
+    }
 ?>
