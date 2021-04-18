1e3bbbb178c7c645560fa4f3114bdbbdc14d8b18
marcelodesouzacorrea <marcelosouzacorrea@hotmail.com>
2021-04-18 14:31:42 -0300

    credencias do banco de dados

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+    // credencias do banco de dados
+    define('DB_SERVER', 'localhost');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', '');
+    define('DB_NAME', 'projeto');
+
+?>
