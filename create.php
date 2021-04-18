91b19fc3dd7bb79864fe5ab1a2d3edaa259bb3f9
marcelodesouzacorrea <marcelosouzacorrea@hotmail.com>
2021-04-18 15:06:36 -0300

    validando o nome

--- create.php
+++ create.php
@@ -5,4 +5,16 @@
     // Defina variáveis ​​e inicialize com valores vazios
 $nome = $inicio = $fim = $valor = $riscos = $Participantes = "";
 $nome_err = $inicio_err = $fim_err = $valor_err = $riscos_err = $Participantes_err = "";
+
+// Processando dados do formulário quando o formulário é enviado
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    // validando o nome
+    $input_name = trim($_POST["nome"]);
+    if(empty($input_nome)){
+        $name_err = "Digite o nome.";
+    } elseif(!filter_var($input_nome, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+        $nome_err = "Digite um nome valido.";
+    } else{
+        $nome = $input_nome;
+    }
 ?>
