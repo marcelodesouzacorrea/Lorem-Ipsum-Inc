ec2df3ae5aaff42979dcf1f6336f7c6bc23e751b
marcelodesouzacorrea <marcelosouzacorrea@hotmail.com>
2021-04-18 16:08:55 -0300

    estrutura html do create.php

--- create.php
+++ create.php
@@ -95,3 +95,78 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         mysqli_close($link);
     }  
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Criar registro</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        .wrapper{
+            width: 600px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                <h2 class="mt-5">Criar registro</h2>
+                <p>Preencha este formulário e envie para adicionar o registro do projeto.</p>
+
+                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                        <div class="form-group">
+                            <label>Nome</label>
+                            <input type="text" name="nome" class="form-control <?php echo (!empty($nome_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $nome; ?>">
+                            <span class="invalid-feedback"><?php echo $nome_err;?></span>
+                        </div>
+
+                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                        <div class="form-group">
+                            <label>Inicio</label>
+                            <input type="text" name="inicio" class="form-control <?php echo (!empty($inicio_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $inicio; ?>">
+                            <span class="invalid-feedback"><?php echo $inicio_err;?></span>
+                        </div>
+
+                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                        <div class="form-group">
+                            <label>Fim</label>
+                            <input type="text" name="fim" class="form-control <?php echo (!empty($fim_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $fim; ?>">
+                            <span class="invalid-feedback"><?php echo $fim_err;?></span>
+                        </div>
+
+                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                        <div class="form-group">
+                            <label>Valor</label>
+                            <input type="text" name="valor" class="form-control <?php echo (!empty($valor_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $valor; ?>">
+                            <span class="invalid-feedback"><?php echo $valor_err;?></span>
+                        </div>
+
+                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                        <div class="form-group">
+                            <label>Riscos</label>
+                            <input type="text" name="riscos" class="form-control <?php echo (!empty($riscos_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $riscos; ?>">
+                            <span class="invalid-feedback"><?php echo $riscos_err;?></span>
+                        </div>        
+                
+                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                        <div class="form-group">
+                            <label>Participantes</label>
+                            <input type="text" name="participantes" class="form-control <?php echo (!empty($participantes_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $participantes; ?>">
+                            <span class="invalid-feedback"><?php echo $participantes_err;?></span>
+                        </div>    
+                        <input type="submit" class="btn btn-primary" value="Submit">
+                        <a href="index.php" class="btn btn-secondary ml-2">Cancelar</a>
+
+                        </form>
+                </div>
+            </div>        
+        </div>
+    </div>
+</body>
+</html>
