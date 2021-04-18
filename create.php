14d462e305e57277236ec5cdd55b777eb3bd74d3
marcelodesouzacorrea <marcelosouzacorrea@hotmail.com>
2021-04-18 15:03:50 -0300

    Defina variáveis ​​e inicialize com valores vazios

--- create.php
+++ create.php
@@ -1,4 +1,8 @@
 <?php
     // incluindo o arquivo config.php
     require_once ('config.php');
+
+    // Defina variáveis ​​e inicialize com valores vazios
+$nome = $inicio = $fim = $valor = $riscos = $Participantes = "";
+$nome_err = $inicio_err = $fim_err = $valor_err = $riscos_err = $Participantes_err = "";
 ?>
