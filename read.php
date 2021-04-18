54b8838277e6883c69d55bbb052e2f93b3679756
marcelodesouzacorrea <marcelosouzacorrea@hotmail.com>
2021-04-18 16:15:48 -0300

    Tente executar a declaração preparada

--- read.php
+++ read.php
@@ -12,4 +12,7 @@
         mysqli_stmt_bind_param($stmt, "i", $param_id);
         // configura o parametro
         $param_id = trim($_GET["id"]);
+        // Tente executar a declaração preparada
+        if(mysqli_stmt_execute($stmt)){
+            $result = mysqli_stmt_get_result($stmt);
 ?>
